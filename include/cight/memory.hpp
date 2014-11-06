edf05d03c625a603d35fa96e83264ed941f7e7e0
helio <helio@roboken.iit.tsukuba.ac.jp>
2014-11-06 15:44:51 +0900

    Added GPL license header
    Signed-off-by: helio <helio@roboken.iit.tsukuba.ac.jp>

--- include/cight/memory.hpp
+++ include/cight/memory.hpp
@@ -1,5 +1,24 @@
-#ifndef DEJAVU_WORKING_MEMORY_HPP
-#define DEJAVU_WORKING_MEMORY_HPP
+/*
+Copyright (c) Helio Perroni Filho <xperroni@gmail.com>
+
+This file is part of Cight.
+
+Cight is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Cight is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Cight. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CIGHT_MEMORY_HPP
+#define CIGHT_MEMORY_HPP
 
 #include <clarus/core/list.hpp>
 
