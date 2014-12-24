d234603f576450e0d252e6796918e316043d2dd4
helio <helio@roboken.iit.tsukuba.ac.jp>
2014-12-24 15:40:38 +0900

    Simplified stream matcher API
    The new stream matcher API defines streams as callables. The end
    of stream is communicated by returning an empty list.
    
    Signed-off-by: helio <helio@roboken.iit.tsukuba.ac.jp>

--- include/cight/stream_matcher.hpp
+++ include/cight/stream_matcher.hpp
@@ -22,78 +22,15 @@ along with Cight. If not, see <http://www.gnu.org/licenses/>.
 
 #include <clarus/core/list.hpp>
 
-#include <boost/smart_ptr.hpp>
+#include <boost/function.hpp>
 
 #include <opencv2/opencv.hpp>
 
 namespace cight {
-    struct StreamMatcher;
+    /**
+    \brief Returns a pair of matched frames from teach (first) and replay (second) streams.
+    */
+    typedef boost::function<clarus::List<cv::Mat>()> StreamMatcher;
 }
-
-struct cight::StreamMatcher {
-    /** \brief Reference-counted smart pointer type. */
-    struct P: boost::shared_ptr<StreamMatcher> {
-        P():
-            boost::shared_ptr<StreamMatcher>()
-        {
-            // Nothing to do.
-        }
-
-        P(StreamMatcher *p):
-            boost::shared_ptr<StreamMatcher>(p)
-        {
-            // Nothing to do.
-        }
-
-        clarus::List<cv::Mat> operator () () {
-            StreamMatcher &matcher = *get();
-            return matcher();
-        }
-    };
-
-    /**
-    \brief Returns a pair of matched frames from the teach (first) and replay (second) streams.
-    */
-    virtual clarus::List<cv::Mat> operator () () = 0;
-
-    /**
-    \brief Collect data from the teach stream.
-
-    Stream matchers should implement this method so that it performs any required buffer
-    initialization tasks related to the teach stream (e.g. loading a first batch of
-    images). This enables user code to intersperse setup tasks with their own required
-    initial operations.
-    */
-    virtual void fillTeachBuffer() = 0;
-
-    /**
-    \brief Collect data from the replay stream.
-
-    Stream matchers should implement this method so that it performs any required buffer
-    initialization tasks related to the replay stream (e.g. loading a first batch of
-    images). This enables user code to intersperse setup tasks with their own required
-    initial operations.
-    */
-    virtual void fillReplayBuffer() = 0;
-
-    /**
-    \brief Compute an initial matching between streams.
-
-    Stream matchers should implement this method so that it computes any required parameters
-    for matching the streams. It should rely on \c fillBuffers() for the initialization
-    of any required buffers, calling it automatically if it hadn't yet been done manually.
-    This enables user code to intersperse setup tasks with their own required initial
-    operations.
-
-    After the first call, implementations should automatically update computed parameters
-    as required.
-    */
-    virtual void computeMatching() = 0;
-
-    /**
-    \brief Returns whether the stream matcher is still active.
-    */
-    virtual bool more() const = 0;
-};
 
 #endif
